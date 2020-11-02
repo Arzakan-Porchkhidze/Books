1251c82df8a40de4fc11b63037e4f34fc3c61f0a
Arzakan <arzakanporchkhidze17@mgail.com>
2020-11-02 16:02:32 +0400

    adding navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        HOME
+                    </li>
+                    <li>
+                        FAVORITES
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
