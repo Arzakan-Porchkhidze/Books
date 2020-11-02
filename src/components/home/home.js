82a65f2b1318b22ddf7dd4b9bcbd04f135ed2654
Arzakan <arzakanporchkhidze17@mgail.com>
2020-11-02 14:54:51 +0400

    creating home page

---
+++ src/components/home/home.js
@@ -0,0 +1,37 @@
+import React, {useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchBooks } from '../../redux/index'
+import BookCard from './bookCard'
+
+function Home() {
+  const bookData = useSelector(state => state);
+  const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchBooks());
+    }, []);
+
+    return bookData.loading ? (
+      <h2>Loading</h2>
+    ): bookData.error ? (
+      <h2>{bookData.error}</h2>
+    ): (
+      <div>
+        <h2 style = {{backgroundColor: 'yellow'}}>Books List</h2>
+        <div>
+          <ul>
+            {bookData.books.items? bookData.books.items
+            .map((item) => <li key={item.id}>
+            <BookCard
+             thumbnail = {item.volumeInfo.imageLinks.thumbnail} 
+             bookTitle = {item.volumeInfo.title}
+             author = {item.volumeInfo.authors[0]}
+            /></li>)
+            : null 
+            }
+          </ul>
+        </div>
+      </div>
+    )
+};
+
+export default Home;
